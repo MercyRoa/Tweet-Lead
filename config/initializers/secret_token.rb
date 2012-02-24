d041d6bbb48fdb92edc5a7bf8612a90960221552
Mercy Roa <mercyroa@hotmail.com>
2012-02-23 20:49:02 -0500

    Primera subida Tweetlead...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetLead::Application.config.secret_token = '0169fbdb67888db07fa05b121d873c480fd4a5ecdf3285179e6ec5877e2d8fdb13cc29552534d225873d492999c3a5edcf977872030325ac331f7051450d536a'
