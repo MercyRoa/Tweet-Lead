90503762950ca229e0751597c39852b762111db6
Mercy Roa <mercy@grupoinnovait.com>
2012-06-12 13:17:54 -0500

    Improving searches code

--- app/models/search.rb
+++ app/models/search.rb
@@ -9,15 +9,14 @@ class Search < ActiveRecord::Base
 
     tweets, data, usernames = [],[],[]
     
-    config = {:rpp => 100, :page => 1, :result_type => "recent" }
+    config = {:rpp => 5, :page => 1, :result_type => "recent" }
     config[:since_id] = self.last_tweet_id unless self.last_tweet_id.nil?  
-
     
     while true
       puts "pagina " + config[:page].to_s
         
       r = a.tc.search(self.search + " -rt -http -#ff", config)
-      if r.empty? || config[:page] == 15 then break end
+      if r.empty? || config[:page] == 2 then break end
       tweets += r 
       config[:page] += 1 
     end
@@ -28,19 +27,13 @@ class Search < ActiveRecord::Base
     
     replies = File.readlines "vendor/replies/replies.txt" 
     
-    tweets.each_with_index do |t,i|
+    tweets.reject { |t| self.repeat_user(t.from_user, usernames) }.each_with_index do |t,i|
       puts "#{i}. Tweet de @" + t.from_user
       reply = "@#{t.from_user} " +  replies[rand(replies.count)]
       
       #check encoding and length
       
       #aply filters
-      
-      #Ckeck that is not a repetead user
-      #@todo Create index
-      next if Profile.exists?(:screen_name => t.from_user) 
-      next if SearchesResult.exists?(:username => t.from_user)
-      next if usernames.include? t.from_user
       
       usernames << t.from_user
       data << SearchesResult.new({
@@ -52,18 +45,18 @@ class Search < ActiveRecord::Base
     
       self.last_tweet_id = t.id
     end
-
-    SearchesResult.import data
-      
-    self.save
-    raise tweets.map{|t| "@" + t.from_user + ": " + t.text}.to_yaml 
+    
+    return if !data.empty?
+    
+    raise data.map{|t| "@" + t.username + ": " + t.tweet}.to_yaml 
   end
   
+  #Ckeck that is not a repetead user, should be called with reject
   # user is string
   # usernames is array
   def repeat_user(user, usernames)
     Profile.exists?(:screen_name => user)    ||
-    SearchResults.exists?(:username => user) ||
+    SearchesResult.exists?(:username => user) ||
     usernames.include?(user)
   end
 
